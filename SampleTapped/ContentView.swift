395fcfc43d18d870599cb06e49cd73fafeaab909
takatoshi.tenko <chanten811@gmail.com>
2023-12-30 19:06:39 +0900

    Initial Commit

---
+++ SampleTapped/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleTapped
+//
+//  Created by 天孝高俊 on 2023/12/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleTapped/SampleTappedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleTappedApp.swift
+//  SampleTapped
+//
+//  Created by 天孝高俊 on 2023/12/30.
+//
+
+import SwiftUI
+
+@main
+struct SampleTappedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
