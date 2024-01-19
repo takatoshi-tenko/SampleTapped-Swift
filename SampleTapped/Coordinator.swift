d67ee8ec6cfd8635f183a41ff6f3d4a184ae0747
takatoshi.tenko <chanten811@gmail.com>
2024-01-19 09:19:31 +0900

    カメラアプリ作成の準備

--- SampleTapped/Coordinator.swift
+++ SampleTapped/Coordinator.swift
@@ -5,4 +5,20 @@
 //  Created by 天孝高俊 on 2024/01/19.
 //
 
-import Foundation
+import SwiftUI
+
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var parent: ImagePicker
+    
+    init(_ parent: ImagePicker) {
+        self.parent = parent
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let uiImage = info[.originalImage] as! UIImage
+        UIImageWriteToSavedPhotosAlbum(uiImage, nil, nil, nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    }
+}
